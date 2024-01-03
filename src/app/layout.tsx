f0a970c2092b5da5bbe70d7b30a27e59b66e20ee
Amir <theamirm@gmail.com>
2024-01-03 05:57:02 +0330

    feat: added metadata for better preview links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import "@/_styles/globals.scss";
 import { Inter } from "next/font/google";
 import type { Metadata, Viewport } from "next";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Amir Bazgir's Portfolio",
-  description: "I'm a Web developer."
+  description: "I'm a fullstack web developer."
 };
 
 export const viewport: Viewport = {
@@ -20,6 +21,47 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Amir Bazgir&apos;s portfolio</title>
+
+        <meta name="title" content="Amir Bazgir's portfolio" />
+        <meta name="description" content="I'm a fullstack web developer." />
+
+        {/* Open Graph / Facebook  */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://b-amir.link" />
+        <meta property="og:title" content="Amir Bazgir's portfolio" />
+        <meta
+          property="og:description"
+          content="I'm a fullstack web developer."
+        />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/b-amir/my-portfolio/main/public/link-preview.png"
+        />
+
+        {/* Twitter  */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="http://b-amir.link" />
+        <meta property="twitter:title" content="Amir Bazgir's portfolio" />
+        <meta
+          property="twitter:description"
+          content="I'm a fullstack web developer."
+        />
+        <meta
+          property="twitter:image"
+          content="https://raw.githubusercontent.com/b-amir/my-portfolio/main/public/link-preview.png"
+        />
+
+        {/* favicon and touch icon */}
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon.png?ebbb82dc3cc1b5eb"
+          type="image/png"
+          sizes="512x512"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
