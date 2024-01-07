07e879377733de3bd19ed352cdbfd838dcd6d112
Amir <theamirm@gmail.com>
2024-01-07 08:18:11 +0330

    refactor: made tooltip more dynamic

--- src/app/_components/Tooltip/index.tsx
+++ src/app/_components/Tooltip/index.tsx
@@ -1,24 +1,85 @@
-import React, { useState } from "react";
 import styles from "./index.module.scss";
+import React, { useLayoutEffect, useRef, useState } from "react";
+
+function TooltipBubble({ text, targetRef }: IBuubleProps) {
+  const bubbleRef = useRef<HTMLDivElement>(null);
+
+  const [bubbleLayout, setBubbleLayout] = useState({
+    position: "bottom",
+    style: {}
+  });
+
+  useLayoutEffect(() => {
+    const bubbleRect = bubbleRef.current?.getBoundingClientRect();
+    const targetRect = targetRef.current?.getBoundingClientRect();
+
+    const spaceAbove = targetRect?.top ?? 0;
+    const spaceBelow = window.innerHeight - (targetRect?.bottom ?? 0);
+
+    if (
+      bubbleRect &&
+      bubbleRect?.height > spaceAbove &&
+      spaceBelow > spaceAbove
+    ) {
+      setBubbleLayout({
+        position: "bottom",
+        style: {
+          marginTop: 30, // for animation effect
+          top: targetRect && targetRect?.bottom - 100
+        }
+      });
+    } else {
+      setBubbleLayout({
+        position: "top",
+        style: {
+          marginBottom: 30, // for animation effect
+          bottom: bubbleRect && bubbleRect?.height - 100
+        }
+      });
+    }
+  }, [targetRef]);
+
+  return (
+    <div
+      ref={bubbleRef}
+      className={
+        bubbleLayout.position === "top"
+          ? styles.tooltipBubbleTop
+          : styles.tooltipBubbleBottom
+      }
+      style={bubbleLayout.style}>
+      {text}
+    </div>
+  );
+}
+
+export const Tooltip = ({ children, text }: ITooltipProps) => {
+  const [show, setShow] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseEnter = () => setShow(true);
+  const handleMouseLeave = () => setShow(false);
+
+  return (
+    <>
+      <span
+        ref={contentRef}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className={styles.tooltipTrigger}>
+        {children}
+      </span>
+      {show && <TooltipBubble targetRef={contentRef} text={text} />}
+    </>
+  );
+};
 
 interface ITooltipProps {
   children: React.ReactNode;
   text: React.ReactNode;
 }
 
-export const Tooltip = ({ children, text }: ITooltipProps) => {
-  const [show, setShow] = useState(false);
-
-  const handleMouseEnter = () => setShow(true);
-  const handleMouseLeave = () => setShow(false);
-
-  return (
-    <div
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      className={styles.tooltipTrigger}>
-      {children}
-      {show && <div className={styles.tooltipBubble}>{text}</div>}
-    </div>
-  );
-};
+interface IBuubleProps {
+  targetRef: React.RefObject<HTMLDivElement>;
+  text: React.ReactNode;
+}
