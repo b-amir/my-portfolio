ebd36b2a211af208b133b827aefb61fca14021c7
Amir <theamirm@gmail.com>
2024-02-21 14:22:08 +0330

    fix: further optimised 3d loading

--- src/app/_sections/HeroSection/Fry3dObject.tsx
+++ src/app/_sections/HeroSection/Fry3dObject.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { getGPUTier } from "detect-gpu";
 import { Suspense, useEffect, useRef, useState, lazy } from "react";
+import { useDebounce } from "@/_hooks/useDebounce";
 
 const Spline = lazy(() => import("@splinetool/react-spline"));
 
@@ -9,6 +10,9 @@ type deviceTiers = "low" | "mobile" | "high" | "loading";
 
 export function Fry3dObject() {
   const [deviceTier, setDeviceTier] = useState<deviceTiers>("loading");
+  const [isLoading, setIsLoading] = useState(true);
+  const visibility = useDebounce(isLoading ? "hidden" : "visible", 200);
+
   const fryObj = useRef();
 
   useEffect(() => {
@@ -31,6 +35,7 @@ export function Fry3dObject() {
 
   function onLoad(splineApp: any) {
     fryObj.current = splineApp;
+    setIsLoading(false);
   }
   function OnError() {
     setDeviceTier("low");
@@ -41,10 +46,11 @@ export function Fry3dObject() {
       {deviceTier === "mobile" && <LowTierImage />}
       {deviceTier === "low" && <LowTierImage />}
       {deviceTier === "high" && (
-        <Suspense fallback={<LowTierImage />}>
+        <Suspense fallback={isLoading ? <LowTierImage /> : null}>
           <Spline
             onLoad={onLoad}
             onError={OnError}
+            style={{ visibility }}
             // scene="https://prod.spline.design/btTGbGyxfpf64yVO/scene.splinecode"
             scene="/fry.splinecode"
           />
