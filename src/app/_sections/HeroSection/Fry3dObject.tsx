8014f7395c8d7de64db459dc93d44b1e454d81ea
Amir <theamirm@gmail.com>
2024-03-27 08:09:17 +0330

    feat: added consent dialogue to show 3d object

--- src/app/_sections/HeroSection/Fry3dObject.tsx
+++ src/app/_sections/HeroSection/Fry3dObject.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 import styles from "./index.module.scss";
 import { useDebounce } from "@/_hooks/useDebounce";
 import { useGpuDetect } from "@/_hooks/useGpuDetect";
+import { FaCheckCircle } from "react-icons/fa";
 import { Suspense, useEffect, useRef, useState, lazy } from "react";
+import { FiBox } from "react-icons/fi";
 
 const Spline = lazy(() => import("@splinetool/react-spline"));
 
@@ -12,6 +14,8 @@ type deviceTiers = "low" | "mobile" | "high" | "loading";
 export function Fry3dObject() {
   const [deviceTier, setDeviceTier] = useGpuDetect();
   const [isLoading, setIsLoading] = useState(true);
+  const [consentGiven, setConsentGiven] = useState("pending");
+
   const visibility = useDebounce(isLoading ? "hidden" : "visible", 200);
 
   const fryObj = useRef();
@@ -34,7 +38,39 @@ export function Fry3dObject() {
     <>
       {deviceTier === "mobile" && <LowTierImage />}
       {deviceTier === "low" && <LowTierImage />}
-      {deviceTier === "high" && (
+      {deviceTier === "high" && consentGiven === "pending" && (
+        <>
+          <LowTierImage />
+          <div className={styles.consentDialog}>
+            <FiBox style={{ fontSize: "xx-large" }} />
+            <p className={styles.consentTitle}>OK to display a 3D object?</p>
+            <div className={styles.consentSubtitle}>
+              <p>Your device is detected to be capable.</p>
+            </div>
+
+            <div className={styles.buttonsRow}>
+              <button
+                onClick={() => {
+                  setConsentGiven("yes");
+                }}
+              >
+                <FaCheckCircle /> Yes
+              </button>
+              <button
+                onClick={() => {
+                  {
+                    setConsentGiven("no");
+                    setDeviceTier("low");
+                  }
+                }}
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+      {deviceTier === "high" && consentGiven === "yes" && (
         <Suspense fallback={isLoading ? <LowTierImage /> : null}>
           <Spline
             onLoad={onLoad}
